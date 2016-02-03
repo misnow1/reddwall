141cf9426a131598478d0eb5151e2450de1c954e
Michael Snow <michael.snow@rackspace.com>
2016-02-03 17:17:53 -0500

    Oopsie

--- .venv/bin/reddwall.py
+++
@@ -1,5 +0,0 @@
-#!/Users/mich8139/workspace/misnow1/reddwall/.venv/bin/python
-# EASY-INSTALL-SCRIPT: 'ReddWall==0.2','reddwall.py'
-__requires__ = 'ReddWall==0.2'
-import pkg_resources
-pkg_resources.run_script('ReddWall==0.2', 'reddwall.py')
